0cc5934975c982ab15e959f4be31d02bed3d3a95
Gunnsteinn <10299301+gunnsteinnskula@users.noreply.github.com>
2026-02-26 19:36:49 +0000

    Lesson 3 first half

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import { Input } from './components/Input'
-import { ShopCard } from './components/ShopCard'
+import "./App.css";
+import { Form } from "./components/Form";
+import { ShopCard } from "./components/ShopCard";
 
 function App() {
-  const [myName, setMyName] = useState('Hjalti')
-  const [email, setEmail] = useState('')
-
   return (
     <>
-      // Búa til card og setja inn props(data)
-      // búa til component button
-      <h2>hello world</h2>
-      <div>myName</div>
-      <div>{myName}</div>
-      <div>
-       <Input value={myName} onChange={(e) => setMyName(e.target.value)} /> 
-       <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        //færa þennan
-        <button onClick={() => alert("submitted:" +email)}>Submit</button>
-      </div>
-
-     {/* Card */}
-     <ShopCard />
-     <ShopCard />
+      <ShopCard />
+      <ShopCard />
+      <Form />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
